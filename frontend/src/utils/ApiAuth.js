0b1a3e9892589078073fcc6f82c0d14fd7698a40
Aleksander Panferov2 <panferov.a.a@yandex.ru>
2023-06-08 11:50:42 +0300

    fix: update URL ApiAuth.js

--- frontend/src/utils/ApiAuth.js
+++ frontend/src/utils/ApiAuth.js
@@ -1,5 +1,5 @@
 const config = {
-  url: "https://api.learning-mesto.nomoredomains.rocks/",
+  url: "https://api.learning-mesto.nomoredomains.rocks",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
